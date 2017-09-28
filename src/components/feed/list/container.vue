50825c9639d2733611303994499ef0a68a92152b
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-09-27 21:27:57 -0400

    delete feeds

--- src/components/feed/list/container.vue
+++ src/components/feed/list/container.vue
@@ -9,12 +9,36 @@
         url
         description
         language
+        title
       }
+    }
+  `
+
+  export const DeleteFeedMutation = gql`
+    mutation DeleteFeed($id: String!){
+      deleteFeed(id: $id)
     }
   `
 
   export default {
     extends: FeedList,
+    methods: {
+      deleteFeed (id) {
+        this.$apollo.mutate({
+          mutation: DeleteFeedMutation,
+          variables: {
+            id: id
+          },
+          update: (store, { data: {deleteFeed} }) => {
+            const data = store.readQuery({query: FeedQuery})
+            data.feeds = data.feeds.filter(item => {
+              return item.id !== deleteFeed
+            })
+            store.writeQuery({query: FeedQuery, data})
+          }
+        })
+      }
+    },
     apollo: {
       feeds () {
         return {
--- src/components/feed/list/index.vue
+++ src/components/feed/list/index.vue
@@ -1,24 +1,60 @@
 <template>
   <div>
     <div v-for="feed in feeds" :class="$style.item">
-      <h3 :class="$style.description">
-        <router-link :to="`/feed/` + feed.id">{{feed.description}}
-        </router-link>
-      </h3>
+
+      <div :class="$style.header">
+        <div>
+          <h3 :class="$style.description">
+            <router-link :to="`/feed/` + feed.id">{{feed.title}}
+            </router-link>
+          </h3>
+        </div>
+        <button :class="$style.button" @click="deleteFeed(feed.id)">
+          <delete-icon/>
+        </button>
+      </div>
+
+
       <div :class="$style.body">
-        <a :href="feed.url" target="_blank">{{feed.url}}</a>
+        <div :class="$style.metaItem">
+          <span :class="$style.icon">
+            <description-icon/>
+          </span>
+          <p>{{feed.description}}</p>
+        </div>
+        <div :class="$style.metaItem">
+          <span :class="$style.icon">
+            <link-icon/>
+          </span>
+          <a :href="feed.url" target="_blank">{{feed.url}}</a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import DeleteIcon from '@/components/icons/deleteIcon.vue'
+import DescriptionIcon from '@/components/icons/descriptionIcon.vue'
+import LinkIcon from '@/components/icons/linkIcon.vue'
+
 export default {
-  name: 'feed-list'
+  name: 'feed-list',
+  components: {
+    DeleteIcon, DescriptionIcon, LinkIcon
+  }
 }
 </script>
 
 <style lang="scss" module>
+
+  .header{
+    display: grid;
+    grid-template-columns: 1fr 50px;
+    background-color: #F0433A;
+    padding: .5rem;
+  }
+
   .item{
     background-color: #eee;
     text-decoration: none;
@@ -34,9 +70,7 @@ export default {
   }
 
   .description{
-    padding: .5rem 2rem;
     margin: 0;
-    background-color: #F0433A;
     color: white;
     font-size: 1.5rem;
     font-weight: 100;
@@ -47,10 +81,51 @@ export default {
   }
 
   .body{
-    padding: .5rem 2rem;
+    padding: .5rem;
 
     a{
       color: #2E112D;
+    }
+  }
+
+  .button{
+    height: 30px;
+    width: 30px;
+    border: 0;
+    background-color: transparent;
+    justify-self: right;
+
+    svg{
+      fill: white;
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .metaItem{
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    background-color: white;
+    padding: .5rem;
+
+    &:not(:last-of-type){
+      margin-bottom: .5rem;
+    }
+
+    p{
+      margin: 0;
+    }
+  }
+
+  .icon{
+    display: block;
+    height: 20px;
+    width: 20px;
+
+    svg{
+      height: 100%;
+      width: 100%;
+      fill: #F0433A;
     }
   }
 </style>
